71c55abbd23bff7c42ea2b906ce53987d5d977b3
Amemiya__ <leandro.amemiya@gmail.com>
2022-03-31 10:39:39 +0900

    initial commit

---
+++ scripts/interface.js
@@ -0,0 +1,54 @@
+let winner;
+let player_1 = prompt("Player 1: ");
+let player_2 = prompt("Player 2: ");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square)=>{
+    square.addEventListener("click", handleClick);
+  });
+
+});
+
+function handleClick(event){
+
+  let square = event.target;
+  let position = square.id;
+
+  if (handleMove(position)) {
+
+    setTimeout(()=>{
+      if (playerTime == 0? winner = player_1 : winner = player_2);
+      playerTime += 1;
+      alert("Game over! - Player: " + playerTime + " (" + winner + ")" + " is win!");
+    }, 10)
+
+  };
+  updateSquare(position);
+
+}
+
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class="${symbol}"></div>`;
+}
+
+
+// Reset Game - Still in development
+function updateSquares() {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square)=>{
+    let position = square.id;
+    let symbol = board[position];
+
+    if(symbol != "") {
+      square.innerHTML = "";
+    }
+
+  });
+};
